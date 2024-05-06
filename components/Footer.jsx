4f81c8621756aa3f67f35c92b1c1697c44f651d4
debcamila <camiladeborah72@gmail.com>
2024-05-06 12:27:23 -0300

    feat: adicionado o footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,23 @@
+import Socials from "./Socials";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          {/* socials */}
+          <Socials
+            containerStyles="flex gap-x-6 mx-auto xl:mx-0 mb-4"
+            iconsStyles="text-primary text-[22px] hover:text-white transition-all"
+          />
+          {/* copyright */}
+          <div className="text-muted-foreground">
+            Copyright &copy; 2024 Deborah Camila.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
