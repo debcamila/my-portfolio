c1a8d150704e878786f9e7e8e043242337269444
debcamila <camiladeborah72@gmail.com>
2024-05-14 16:05:29 -0300

    fix: resolve problema do espaçamento do titulo da pagina sobre mim

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,7 +2,7 @@ import DevImg from "@/components/DevImg";
 
 const About = () => {
   return (
-    <section className="xl:h-[860px] pb-12 xl:py-24">
+    <section className="min-h-screen pt-12">
       <div className="container mx-auto">
         <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
           Sobre mim
